05094d034d249783190ff57175cf44fd175f3276
Gabriel <gabriel@gabrielhorvat.com>
2022-07-04 18:06:56 +0200

    CoinFlip

--- contracts/1. CoinFlip/CoinFlipHelper.ts
+++ contracts/1. CoinFlip/CoinFlipHelper.ts
@@ -2,7 +2,14 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 const helper = async (victim: any, attacker: any) => {
-  // add code here that will help you pass the test
+  let wins = await victim.consecutiveWins();
+
+  while (wins < 10) {
+    await attacker.hackContract();
+    wins = await victim.consecutiveWins();
+  }
+
+  await victim.consecutiveWins();
 };
 
 export default helper;
